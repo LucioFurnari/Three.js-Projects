8292ecd42452625d7eb22a0fbc29eab314ae36ca
LucioFurnari <luciofurnari99@gmail.com>
2024-09-09 17:00:34 -0300

    feat: add comments, and texture for the planet

--- Project_02/index.js
+++ Project_02/index.js
@@ -3,18 +3,24 @@ import { OrbitControls } from 'jsm/controls/OrbitControls.js';
 
 const w = window.innerWidth;
 const h = window.innerHeight;
+// Create scene
 const scene = new THREE.Scene();
+// Create camera
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
+// Create render instance
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
+// Create orbital controls
 new OrbitControls(camera, renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
+const loader = new THREE.TextureLoader();
+
+const geometry = new THREE.IcosahedronGeometry(1, 12);
 const material = new THREE.MeshStandardMaterial({
-  color: 0xffff00,
+  map: loader.load('./textures/00_earthmap1k.jpg'),
 });
 const earthMesh = new THREE.Mesh(geometry, material);
 scene.add(earthMesh);
@@ -23,6 +29,7 @@ scene.add(earthMesh);
 const hemiLight = new THREE.HemisphereLight( 0xffffbb, 0x080820, 4)
 scene.add(hemiLight);
 
+// Create animation for frame
 function animate() {
   requestAnimationFrame(animate);
 
