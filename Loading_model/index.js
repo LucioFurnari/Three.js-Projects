826a5d5601b9ed54ad72f9aec867c42981a0157a
LucioFurnari <luciofurnari99@gmail.com>
2024-09-11 23:49:46 -0300

    add shadow cast to the scene and sphere model

--- Loading_model/index.js
+++ Loading_model/index.js
@@ -9,6 +9,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true;
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
@@ -18,9 +19,8 @@ const ambientLight = new THREE.AmbientLight(0x404040, 20);
 scene.add(ambientLight);
 
 // Directional Light
-const sunLight = new THREE.DirectionalLight(0x404040, 10);
+const sunLight = new THREE.DirectionalLight(0x404040, 100);
 sunLight.castShadow = true;
-console.log(sunLight)
 scene.add(sunLight);
 
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -29,20 +29,36 @@ controls.update();
 // Add plane
 const planeMat = new THREE.MeshStandardMaterial({
   color: 0xffffff,
-  side: THREE.DoubleSide
+  side: THREE.DoubleSide,
 });
 const planeMesh = new THREE.PlaneGeometry(10,10);
 const plane = new THREE.Mesh(planeMesh, planeMat);
+plane.receiveShadow = true;
 plane.rotateX(-Math.PI * 0.5);
 plane.position.y = -1
 scene.add(plane)
+
+// Add sphere
+const sphereMat = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+});
+const sphereMesh = new THREE.SphereGeometry(1);
+const sphere = new THREE.Mesh(sphereMesh, sphereMat);
+sphere.castShadow = true
+sphere.position.y = 1
+sphere.position.x = 2
+scene.add(sphere);
 
 // Add loader for 3d model (GLTF format)
 const loader = new GLTFLoader();
 loader.setPath('./assets/models/')
 loader.load('scene.gltf', (gltf) => {
   gltf.scene.position.y = 0.2;
+  gltf.scene.castShadow = true
   scene.add(gltf.scene);
+  gltf.scene.traverse(child => {
+    if (child.isObject3D ) child.castShadow = true
+  })
 }, undefined, (error) => {
   console.error(error)
 })
